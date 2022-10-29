c985233e96f94e32edc3223e44fa44dec059b7ca
Thomas11112222 <13290943635@163.com>
2022-10-29 15:18:37 +0800

    github-first-commit

---
+++ src/main/java/com/atguigu/github/gitHubTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.github;
+
+public class gitHubTest {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+        System.out.println("hello github");
+        System.out.println("hello github");
+        System.out.println("hello github");
+        System.out.println("hello github");
+    }
+
+}
